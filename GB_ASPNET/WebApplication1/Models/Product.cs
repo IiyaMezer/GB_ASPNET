48d32da5c0e029c3013d1365b5c2fda98c4133fa
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-10 21:30:26 +0700

    132

--- GB_ASPNET/WebApplication1/Models/Product.cs
+++ GB_ASPNET/WebApplication1/Models/Product.cs
@@ -1,10 +1,12 @@
-﻿namespace WebApplication1.Models
+﻿using System.Data.SqlTypes;
+
+namespace WebApplication1.Models
 {
     public class Product: BaseModel
     {
         public int Cost { get; set; }
         public int GroupId { get; set; }
         public virtual ProductGroup? ProductGroup { get; set; }
-
+        public virtual List<ProductStorage> Products { get; set; } = new List<ProductStorage>();
     }
 }
---
+++ GB_ASPNET/WebApplication1/Models/Storage.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Storage : BaseModel
+    {
+        public  Product  Product{ get; set; }
+        public int ProductId { get; set; }
+
+    }
+}
