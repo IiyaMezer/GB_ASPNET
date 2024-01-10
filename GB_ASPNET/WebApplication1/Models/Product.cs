02b5b38e0ea8628828d13230848f366010a6ef28
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-10 20:55:07 +0700

    123

---
+++ GB_ASPNET/WebApplication1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product: BaseModel
+    {
+        public int Cost { get; set; }
+        public int GroupId { get; set; }
+        public virtual ProductGroup? ProductGroup { get; set; }
+
+    }
+}
---
+++ GB_ASPNET/WebApplication1/Models/ProductGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class ProductGroup: BaseModel
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
