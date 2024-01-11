1b0d8292395ff80813affca89f5460c596acd31d
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-12 06:37:42 +0700

    del trash

--- GB_ASPNET/WebApplication1/Models/Storage.cs
+++ GB_ASPNET/WebApplication1/Models/Storage.cs
@@ -1,10 +1,10 @@
-﻿namespace WebApplication1.Models;
+﻿using WebApplication1.Models.Base;
+
+namespace WebApplication1.Models;
 
 public class Storage : BaseModel
 {
-    public virtual List<Product> Products { get; set; }
-    public  Product  Product{ get; set; }
-    public int ProductId { get; set; }
+    public virtual List<Product> Products { get; set; } = null!;
     public int Count { get; set; }
 
 }
