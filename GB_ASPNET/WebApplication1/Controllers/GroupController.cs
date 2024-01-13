5344072e98f3c4830a055c5988126106509491a7
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-13 20:33:16 +0700

    group controller ready

--- GB_ASPNET/WebApplication1/Controllers/GroupController.cs
+++ GB_ASPNET/WebApplication1/Controllers/GroupController.cs
@@ -1,6 +1,41 @@
-﻿namespace WebApplication1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+
+namespace WebApplication1.Controllers
 {
-    public class GroupController
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
     {
+
+        [HttpDelete("deleteGroup/{groupId}")]
+        public IActionResult DelGroup([FromQuery] int groupId)
+        {
+            try
+            {
+                using (var context = new StoreContext())
+                {
+                    if (context.Groups.Any(x => x.Id == groupId))
+                    {
+                        Group group = context.Groups.FirstOrDefault(x => x.Id == groupId);
+                        context.Remove(group);
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        StatusCode(404);
+                    }
+
+                }
+            }
+            catch
+            {
+
+                return StatusCode(500);
+            }
+            return Ok();
+
+        }
     }
 }
