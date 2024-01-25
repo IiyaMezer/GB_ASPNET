bf63e1d914e8996b9559e418f5f015b90397aef2
Ilya_mezer <mr.mezer@myttk.ru>
2024-01-25 14:47:49 +0700

    123

---
+++ GB_ASPNET/SEM4_Swagger/Services/UserAuth.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEM4_Swagger.Services
+{
+    public class UserAuth
+    {
+        
+    }
+}
